8a75a011d28eb5a71a9f46e93ff3f4cce68918d9
Lee <gdpp4352@gmail.com>
2021-03-18 11:22:30 -0600

    pep8

--- compare.py
+++ compare.py
@@ -1,6 +1,7 @@
 # compare.py
 #
-# This script runs the comparison between timeseries data as specified in config.yaml
+# This script runs the comparison between timeseries data, 
+# as specified in config.yaml
 #
 # Joseph Lee <joseph.lee@pnnl.gov>
 
@@ -23,19 +24,11 @@ config_file = str(pathlib.Path(os.getcwd()).parent) + '/config_test.yaml'
 
 sys.path.append('.')
 
-# Check options and load config
-# if len(sys.argv) < 2:
-#   print("Usage: python compare.py <config.yaml>")
-#   quit()
-
 #conf = yaml.load(open(sys.argv[1]), Loader=yaml.FullLoader)
 conf = yaml.load(open(config_file), Loader=yaml.FullLoader)
 
 base = conf['base']
 comp = conf['comp']
-
-# FIXME: validate configuration file and give nice errors
-#        if something (necessary) is missing
 
 # Load the module t class with the name s
 def get_module_class(t,s):
@@ -52,47 +45,6 @@ def apply_trans(ts,modlist):
     ts = m.apply(ts)
 
   return ts
-
-def get_dap_file(path,config):
-
-  api_url = "https://dteuqmpcac.execute-api.us-west-2.amazonaws.com/test/request-data"
-  out_dir = config["cache_dir"] + "/dap/" + path  
-  credentials = base64.b64encode("%s:%s" % (config["login"], config["pass"]))
-  auth = {"Authorization": "Basic %s" % credentials}
-
-  params = {
-    "output": "json",
-    "filter": {
-        "Dataset": path
-        #"file_type": ["txt"],
-        #"date_time": {
-        #    "between": ["20160505000000", "20160507000000"]
-        #}
-    }
-  }
-
-  print(params)
-
-  req = requests.post(api_url, headers=auth, data=json.dumps(params), verify=False)
-  print(req.text)
-
-# Figure out where the file is (local or remote)
-# and fetch if necessary
-def get_file(path,remote):
-
-  proto = None
-  m = re.match(r"([a-z]+)://(.*)",path,re.IGNORECASE)
-  
-  if m:
-    proto = m.group(1)
-    path = m.group(2)
-  else:
-    proto = "local"
-
-  if proto == "local":
-    return path
-  elif proto == "dap":
-    return get_dap_file(path,remote["dap"])
 
 # Pre-load all the metric modules into an array
 metrics = [get_module_class("metrics",m)() for m in conf["metrics"]]
@@ -111,7 +63,8 @@ for lev in conf['levels']['height_agl']:
 
     print()
     print('######################### height a.g.l.: '+str(lev)+\
-        ' '+conf['levels']['height_units']+' #########################')
+        ' '+conf['levels']['height_units']+' #########################'
+        )
     print()
 
     # Load the data and compute the metrics
@@ -119,10 +72,10 @@ for lev in conf['levels']['height_agl']:
 
     print('********** for '+base['name']+': **********')
 
-    base["path"] = get_file(base["path"], None) # local files
-
     # run __init__
-    base["input"] = get_module_class("inputs",base["format"])(base["path"],base["var"], base['target_var'])
+    base["input"] = get_module_class("inputs",base["format"])(base["path"], \
+        base["var"], base['target_var']
+        )
 
     base["data"] = base["input"].get_ts(lev, base['freq'], base['flag'])
 
@@ -131,14 +84,19 @@ for lev in conf['levels']['height_agl']:
         print()
         print('********** for '+c['name']+': **********')
 
-        c["path"] = get_file(c["path"], None) # local files
         # run __init__
-        c["input"] = get_module_class("inputs",c["format"])(c["path"],c["var"],c['target_var'])
+        c["input"] = get_module_class("inputs", c["format"])(c["path"], \
+            c["var"],c['target_var']
+            )
 
-        c["data"] = c["input"].get_var_ts(conf["location"], lev, c['freq'], c['flag'])
+        c["data"] = c["input"].get_var_ts(conf["location"], lev, c['freq'], \
+            c['flag']
+            )
 
-        results.append({'truth name': base['name'], 'model name': c['name'], "path": c["path"], \
-                        "location": conf["location"], "var": c["var"]})
+        results.append({'truth name': base['name'], 'model name': c['name'], \
+                        "path": c["path"], \
+                        "location": conf["location"], "var": c["var"]}
+                        )
 
         combine_df = crosscheck_ts.align_time(base["data"], c["data"])
 
@@ -146,10 +104,13 @@ for lev in conf['levels']['height_agl']:
 
         only_na = combine_df[~combine_df.index.isin(compute_df.index)]
         print()
-        print('to calculate metrics, removing the following time steps that contain NaN values:')
+        print('to calculate metrics, removing the following time steps'+\
+            ' that contain NaN values:'
+            )
         print(only_na.index.strftime("%Y-%m-%d %H:%M:%S").values)
 
-        # for future purposes, in case of reading in mulitple compare data columns
+        # for future purposes, 
+        # in case of reading in mulitple compare data columns
         for pair in itertools.combinations(compute_df.columns, 2): 
 
             # baseline should be the 1st (Python's 0th) column
@@ -158,7 +119,9 @@ for lev in conf['levels']['height_agl']:
 
         if len(x) != len(y): 
 
-            sys.exit('Lengths of baseline and compare datasets are not equal!')
+            sys.exit('Lengths of baseline and compare datasets are'+\
+                ' not equal!'
+                )
 
         for m in metrics: 
 
@@ -166,7 +129,8 @@ for lev in conf['levels']['height_agl']:
 
         print()
         print(conf['plot']['var']+' metrics: '+c['name']+' - '+base['name']+\
-            ' at '+str(lev)+' '+conf['levels']['height_units'])
+            ' at '+str(lev)+' '+conf['levels']['height_units']
+            )
         print()
         for key, val in results[0].items():
             if key != 'path': 
